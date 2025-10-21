fb0c13c006cd5a9e40508055f04ef4a872b0dfc3
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2025-10-20 22:46:41 -0400

    parser block tests

--- test/dtest_test_parser.cpp
+++ test/dtest_test_parser.cpp
@@ -31,5 +31,32 @@ TEST_CASE("Parser Analysis") {
     }
 
     SUBCASE("Parse Block") {
+        acc::lexer lxr(R"(
+                int y = 3;
+                {
+                    int x : mut = 4;
+                };
+            )",
+                       acc::globals::ACC_DELIMS,
+                       acc::globals::ACC_PAIR_DELIMS,
+                       acc::globals::ACC_KW_SET,
+                       acc::globals::ACC_KW_TYPE_SET);
+
+        auto ts = lxr.lex();
+        acc::acc_parser prs(ts);
+        auto v = prs.parse();
+        const auto* var_y = std::get<acc::node::DeclarationStmt*>(v[0]);
+        CHECK(var_y->name.word == "y");
+        CHECK(var_y->type.word == "int");
+        CHECK(var_y->has_const(var_y->cv_qual_flags));
+        CHECK(!var_y->has_volatile(var_y->cv_qual_flags));
+        CHECK(acc::interp::expr_eval{}.as<int>(var_y->expr.value()) == 3);
+
+        const auto* block = std::get<acc::node::BlockStmt*>(v[1]);
+        const auto* blx = std::get<acc::node::DeclarationStmt*>(block->stmts[0]);
+        CHECK(blx->name.word == "x");
+        CHECK(blx->type.word == "int");
+        CHECK(!blx->has_const(blx->cv_qual_flags));
+        CHECK(acc::interp::expr_eval{}.as<int>(blx->expr.value()) == 4);
     }
 }
